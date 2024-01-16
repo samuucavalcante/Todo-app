12355645bebc14ce9d345070ff6f629a2ef8e642
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2024-01-16 13:19:11 -0300

    feat: create logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,17 @@
+import { type ComponentProps } from "react";
+import { Share_Tech_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Share_Tech_Mono({ weight: ["400"] });
+
+type LogoProps = ComponentProps<"h3"> & {};
+export function Logo({ ...props }: LogoProps) {
+  return (
+    <h3 className={cn(font.className,props.className, "text-center break-normal  overflow-hidden text-nowrap md:text-xl")} {...props}>
+      {"</>"} Samuel Cavalcante
+      <span className="block" {...props}>
+        {"{"}To Do{"}"}
+      </span>
+    </h3>
+  );
+}
