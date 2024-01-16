758906dc8ac33931779aa62c4ab584ab022ca6cf
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2024-01-16 13:19:50 -0300

    feat: create task component

---
+++ src/components/task.tsx
@@ -0,0 +1,96 @@
+import { cn } from "@/lib/utils";
+import { type ComponentProps } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+{
+  /* <TaskRoot>
+      <TaskTitle skTitle>{title}</TaskTitle>
+      <TaskContent>{content}</TaskContent>
+      <TaskFooter>
+          <TaskStatus>
+              {status}
+          </TaskStatus>
+          <TaskActions>
+              <TaskIcon>{icon}</TaskIcon>
+              <TaskIcon>{icon}</TaskIcon>
+          </TaskActions>
+      </TaskFooter>
+  </TaskRoot> */
+}
+
+type TaskProps = ComponentProps<"div"> & {};
+export function Task({ ...props }: TaskProps) {
+  return (
+    <div
+      className={cn(
+        props.className,
+        "border-1 items-stretch border-slate-800 h-44 flex flex-col bg-[#333233] p-2 rounded-md"
+      )}
+      {...props}
+    />
+  );
+}
+
+type TaskTitleProps = ComponentProps<"h2"> & {};
+export function TaskTitle({ ...props }: TaskTitleProps) {
+  return (
+    <h2 className={cn(props.className, "text-xl font-semibold")} {...props} />
+  );
+}
+
+type TaskContentProps = ComponentProps<"div"> & {};
+export function TaskContent({ ...props }: TaskContentProps) {
+  return <div className={cn(props.className, "text-white/50")} {...props} />;
+}
+
+type TaskFooterProps = ComponentProps<"div"> & {};
+export function TaskFooter({ ...props }: TaskFooterProps) {
+  return (
+    <div
+      className={cn(props.className, "mt-auto flex justify-between")}
+      {...props}
+    />
+  );
+}
+type TaskActionsProps = ComponentProps<"div"> & {};
+export function TaskActions({ ...props }: TaskActionsProps) {
+  return (
+    <div className={cn(props.className, "flex flex-row gap-2")} {...props} />
+  );
+}
+type TaskIconProps = ComponentProps<"div"> & {
+  label?: string;
+};
+export function TaskIcon({ ...props }: TaskIconProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <div
+            className={cn(
+              props.className,
+              "text-[#b7bdc8] text-sm cursor-pointer"
+            )}
+            {...props}
+          />
+        </TooltipTrigger>
+        <TooltipContent>{props.label}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+type TaskStatusProps = ComponentProps<"div"> & {};
+export function TaskStatus({ ...props }: TaskStatusProps) {
+  return (
+    <div
+      className={cn(props.className, "mt-auto flex justify-between")}
+      {...props}
+    />
+  );
+}
